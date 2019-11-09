1da45ae8f43fc5b73b83242d1d157ea4ab2f93e8
李铭昕 <715557344@qq.com>
2019-11-09 17:45:51 +0800

    Update user_controller.go

--- beego/controller/user_controller.go
+++ beego/controller/user_controller.go
@@ -8,12 +8,11 @@ type UserController struct {
 }
 
 func (this *UserController) Get() {
+	id := this.Ctx.Input.Param(":id")
 	o := orm.NewOrm()
 	var maps []orm.Params
-	o.Raw("SELECT * FROM `user` WHERE id = 1 LIMIT 1;").Values(&maps)
-	json := map[string]interface{}{"code":0, "data":maps[0]}
+	o.Raw("SELECT * FROM `user` WHERE id = ? LIMIT 1;", id).Values(&maps)
+	json := map[string]interface{}{"code": 0, "data": maps[0]}
 	this.Data["json"] = &json
 	this.ServeJSON()
 }
-
-
