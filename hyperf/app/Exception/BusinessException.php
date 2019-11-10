90b27f79b0f23ecd728ec1c44efaabf6f911eec2
李铭昕 <715557344@qq.com>
2019-11-10 13:28:58 +0800

    Deleted useless code.

--- hyperf/app/Exception/BusinessException.php
+++ hyperf/app/Exception/BusinessException.php
@@ -12,7 +12,6 @@ declare(strict_types=1);
 
 namespace App\Exception;
 
-use App\Constants\ErrorCode;
 use Hyperf\Server\Exception\ServerException;
 use Throwable;
 
@@ -20,10 +19,6 @@ class BusinessException extends ServerException
 {
     public function __construct(int $code = 0, string $message = null, Throwable $previous = null)
     {
-        if (is_null($message)) {
-            $message = ErrorCode::getMessage($code);
-        }
-
         parent::__construct($message, $code, $previous);
     }
 }
--- hyperf/app/Model/Model.php
+++ hyperf/app/Model/Model.php
@@ -13,10 +13,7 @@ declare(strict_types=1);
 namespace App\Model;
 
 use Hyperf\DbConnection\Model\Model as BaseModel;
-use Hyperf\ModelCache\Cacheable;
-use Hyperf\ModelCache\CacheableInterface;
 
-abstract class Model extends BaseModel implements CacheableInterface
+abstract class Model extends BaseModel
 {
-    use Cacheable;
 }
